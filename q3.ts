70f5089da08b22cd0c545d8307da91ce9be8fc7a
PanatKaewlangka <panut4207315@gmail.com>
2024-07-01 18:22:20 +0700

    q3

--- q3.ts
+++ q3.ts
@@ -1,20 +1,17 @@
 function primeNumber(a) {
   let n = 0;
-  for (let i = a; i > 1; i--) {
-    if (a % i === 0) {
-      n++;
-    }
+  for (let i = 2; i < a; i++) {
+      if (a % i === 0) {
+          n++;
+      }
   }
   if (n > 0) return "No";
   else return "Yes";
 }
-
-const d1 = 10;
-const d2 = 29;
-const d3 = 2;
-
+var d1 = 10;
+var d2 = 29;
+var d3 = 2;
 console.log(primeNumber(d1));
 console.log(primeNumber(d2));
 console.log(primeNumber(d3));
-
 module.exports = primeNumber;
